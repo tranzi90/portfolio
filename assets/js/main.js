ac451bd00ed5d4f510bdd1f50c4dbcf6b0927c95
denis_levchenko <tranzi90@gmail.com>
2021-08-25 22:02:27 +0300

    work with pictures++

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-/*===== MENU SHOW =====*/ 
+/*===== MENU SHOW =====*/
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -12,6 +13,7 @@ const showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle','nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
+
 const navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
