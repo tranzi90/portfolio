d925ddb97225971e289f95a78096cea6cb4cf913
denis_levchenko <tranzi90@gmail.com>
2021-09-12 21:51:07 +0300

    refactoring+++

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,12 @@ const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
   /*Active link*/
+
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
   
   /*Remove menu mobile*/
+
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
